75e03bf4843a2ce1d1ea35bbdd1b6e357a5b879e
Daniel Dutton <DannyDuttster@gmail.com>
2021-06-04 17:59:44 +0100

    Initial Commit

---
+++ MySmarty/MySmartyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySmartyApp.swift
+//  MySmarty
+//
+//  Created by Daniel Dutton on 04/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct MySmartyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
